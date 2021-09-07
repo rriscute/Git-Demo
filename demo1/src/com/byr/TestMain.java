004a1eb6978f51c63a79c2de96d3e4f03b10d007
rriscute <1804630503@qq.com>
2021-09-07 10:42:58 +0800

    第二次更改

--- demo1/src/com/byr/TestMain.java
+++ demo1/src/com/byr/TestMain.java
@@ -3,5 +3,6 @@ package com.byr;
 public class TestMain {
     public static void main(String[] args) {
         System.out.println("hello , git");
+        System.out.println("hello,java");
     }
 }
